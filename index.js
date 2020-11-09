9018c3459c86f5aa6d9fe5940570e6934320ecd4
Shez Rahman <shezpc@gmail.com>
2020-11-08 19:56:55 -0800

    complete, all functions working

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 $(function () {
-    $("#js-shopping-list-form").on('submit', event => {
-        event.preventDefault();
-        /*
-        take in the value from the input
-        use it to create another shop item list object
-        */
-        itemName = $(this).find('input[name="shopping-list-entry').val();
-        $("ul.shopping-list").append(`
+  $("#js-shopping-list-form").on('submit', event => {
+    event.preventDefault();
+    /*
+    take in the value from the input
+    use it to create another shop item list object
+    */
+    itemName = $(this).find('input[name="shopping-list-entry').val();
+    $("ul.shopping-list").append(`
        <li><span class="shopping-item">${itemName}</span>
        <div class="shopping-item-controls">
        <button class="shopping-item-toggle">
@@ -19,25 +19,21 @@ $(function () {
      </li>`);
 
 
-    })
-})
-
-/*make functions for check and delete*/
-
-$('.shopping-item-toggle').each(function () {
-    var $thisButton = $(this);
-    //$thisButton.on('click', event => { //does not work for dynamically created items
-    $('ul.shopping-list').on('click', '.shopping-item-toggle', function(event){
-      event.stopPropagation();
-        $thisButton.parents("li").find("span.shopping-item").toggleClass('shopping-item__checked');
-    })
-}
-)
-
-
-$('.shopping-item-delete').each(function() {
-  var $thisButton = $(this);
-  $thisButton.on('click', event => {
-    $thisButton.parents("li").remove();
   })
+
+  /*make functions for check and delete*/
+
+
+  //$thisButton.on('click', event => { //does not work for dynamically created items
+  $('ul.shopping-list').on('click', '.shopping-item-toggle', function (event) {
+    $(this).parents("li").find("span.shopping-item").toggleClass('shopping-item__checked');
+  })
+
+
+
+  $('ul.shopping-list').on('click', '.shopping-item-delete', function (event) {
+    $(this).parents("li").remove();
+  }
+
+  )
 })
